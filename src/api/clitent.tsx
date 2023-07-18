ff17a0e1c4809a8886bcd0223b5561abe58a6c60
윤선용 <sunyong9059@naver.com>
2023-07-19 02:52:46 +0900

    Refactor : client 수정
    window 캐쉬를 이용해서 중복 키워드 api 요청 막아둠

--- src/api/clitent.tsx
+++ src/api/clitent.tsx
@@ -1,9 +1,19 @@
-import axios from "axios";
+import axios from 'axios';
 
-const BASE_URL = 'http://localhost:4000/sick';
-
-const client = axios.create({
-    baseURL : BASE_URL,
-})
-
-export default client;
+const getData = async (keyword: string) => {
+  if ('caches' in window) {
+    const cacheStorage = await caches.open('sick');
+    const cachedResponse = await cacheStorage.match(keyword);
+    if (!cachedResponse) {
+      console.info('calling api');
+      const response = await axios.get(`http://localhost:4000/sick?q=${keyword}`)
+      const store = response.data;
+      cacheStorage.put(keyword, new Response(JSON.stringify(store)));
+      return store;
+    }
+    const cached = await cachedResponse?.json();
+    return cached;
+  }
+  return [];
+};
+export default getData;
