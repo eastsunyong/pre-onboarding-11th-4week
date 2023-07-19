eaf4e8b5ec9d8a789e4be73f039b84a3c852c6ff
윤선용 <sunyong9059@naver.com>
2023-07-19 14:07:18 +0900

    Style : NoSearch 스타일 수정

--- src/components/NoSearch.style.ts
+++ src/components/NoSearch.style.ts
@@ -1,5 +1,10 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
   height: 40px;
+  width: 440px;
+  box-shadow: 0px 2px 8px rgba(17, 24, 39, 0.25);
 `;
